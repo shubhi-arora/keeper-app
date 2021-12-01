d2cebfa097ce60837f668da7c80fccc3704d8cc1
shubhi-arora <arora.shubhi0565@gmail.com>
2021-12-01 19:16:40 +0530

    Change in index.html file

---
+++ build/precache-manifest.f475d6c4a6a1529359f6aa6bfd8dcb40.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5fa57eb2063690e23beb4c33b8e9f9c",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "45e43f086e355747d36f",
+    "url": "/keeper-app/static/js/2.4d2a2fcb.chunk.js"
+  },
+  {
+    "revision": "ac8f4da79ec1b71a771b",
+    "url": "/keeper-app/static/js/main.e6af266b.chunk.js"
+  },
+  {
+    "revision": "e4ad95b7ee73d89007ed",
+    "url": "/keeper-app/static/js/runtime-main.fc0473d0.js"
+  }
+]);
