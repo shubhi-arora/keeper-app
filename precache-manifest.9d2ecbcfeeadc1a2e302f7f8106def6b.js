7f4a1e610230e06c7a35c0ad0f15038a5daaef98
shubhi-arora <arora.shubhi0565@gmail.com>
2021-12-01 19:17:34 +0530

    Updates

---
+++ precache-manifest.9d2ecbcfeeadc1a2e302f7f8106def6b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0267325524c851ee2d280199c74bb685",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "45e43f086e355747d36f",
+    "url": "/keeper-app/static/js/2.4d2a2fcb.chunk.js"
+  },
+  {
+    "revision": "ac8f4da79ec1b71a771b",
+    "url": "/keeper-app/static/js/main.e6af266b.chunk.js"
+  },
+  {
+    "revision": "e4ad95b7ee73d89007ed",
+    "url": "/keeper-app/static/js/runtime-main.fc0473d0.js"
+  }
+]);
